b70cc57e57af50948a67904c395ad90b2fbc3f37
alirezape40 <73391246+alirezape40@users.noreply.github.com>
2020-10-24 09:08:31 -0700

    Delete index.js

--- پروژه 1/js/index.js
+++
@@ -1,8 +0,0 @@
-function mymenu() {
-    var x = document.getElementById('menu');
-    if (x.className === "header-menu") {
-        x.className += " show";
-    } else {
-        x.className = "header-menu";
-    }
-}
